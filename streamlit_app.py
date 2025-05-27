7dcc7585f6ada34e09e8e2e1af36bb3f3d567f6c
fararabela <dindaybella@gmail.com>
2025-05-27 09:46:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 
 
 if (angka % 2)== 0:
-    st.write(f"{angka} Adalah Bilangan Genap")
+    st.write(f"{angka} adalah Bilangan Genap")
 else:
     st.write(f"{angka} adalah Bilangan Ganjil")
     
